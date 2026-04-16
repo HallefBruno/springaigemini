5114224b1ed104df532e050e469e5125ec65f053
hallefbruno <brunohallef@gmail.com>
2026-04-16 10:47:20 -0300

    Ignore certificado

--- src/main/java/com/ia/aispring/beans/GeminiConfig.java
+++ src/main/java/com/ia/aispring/beans/GeminiConfig.java
@@ -1,9 +1,17 @@
 package com.ia.aispring.beans;
 
 import com.google.genai.Client;
+import javax.net.ssl.SSLContext;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.core5.ssl.SSLContexts;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 @Configuration
 public class GeminiConfig {
@@ -17,7 +25,20 @@ public class GeminiConfig {
 			.apiKey(apiKey)
 			.build();
     }
+	
+	@Bean
+	public RestClientCustomizer restClientCustomizer() throws Exception {
+		SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, (cert, authType) -> true).build();
+
+		org.apache.hc.client5.http.impl.classic.CloseableHttpClient httpClient = HttpClients.custom()
+			.setConnectionManager(PoolingHttpClientConnectionManagerBuilder.create()
+				.setTlsSocketStrategy(new DefaultClientTlsStrategy(sslContext, NoopHostnameVerifier.INSTANCE))
+				.build())
+			.build();
+
+		return restClientBuilder -> {
+			restClientBuilder.requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
+		};
+	}
+
 }
-
-
-
